9fcc3824e85ef206208bb04b0d656c314e7a5796
Bjorn Remseth <la3lma@gmail.com>
2012-03-22 08:33:55 +0100

    build and test everything, not nicely, but it does the job

--- bin/buildall.sh
+++ bin/buildall.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-clisp -c lisp/adv.lisp
+
+# clisp -c src/adv.lisp
+#
+#  XXX Current objective:
+#      1) Make a script that compiles and runs everything
+#         including regression tests.
+#      2) Make it nice, ideomatically correct, using quicklisp and adv.
+#
+
+## This should have been sufficient but isn't
+ clisp -q -x '(asdf:compile-system :adv)'
+
+## So instead we do this
+clisp -q -c "src/adv.lisp"
+clisp -q -c "src/adv-regression.lisp"
+
+## And run regression tests
+clisp  -q -x '(load "src/adv")(load "src/adv-regression.lisp")(in-package :adv-regression)(run-tests)'
+
