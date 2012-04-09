382f73007708da1d7a7283d643fb974089bcf29d
Bjorn Remseth <la3lma@gmail.com>
2012-04-09 15:09:37 +0200

    Reshuffle a bit, make the initializer a parameter of the testrunner

--- src/adv-regression.lisp
+++ src/adv-regression.lisp
@@ -9,9 +9,34 @@
 
 (require 'adv) ;; ???
 
-;; Documentation can be found her
-;; file:///Users/rmz2/quicklisp/dists/quicklisp/software/lisp-unit-20120107-git/documentation/lisp-unit.html#overview
+(defun run-testgame ()
+  "Run a test game interactively"
+  (let* ((my-world     (initialize-fixture))
+	 (player       (adv:find-player "player" my-world)))
+    (game-repl player)))
 
+
+(defun run-command-oneliner (initializer inputstring expected-output &key (tracep nil))
+  "Run a sequence of game commands encoded in the inputstream and return the output from the game as a string"
+  (let* ((inputstream (make-string-input-stream (format nil "~a ~%quit~%" inputstring)))
+         (outputstream (make-string-output-stream))
+	 (my-world     (funcall initializer :input inputstream :output outputstream))
+	 (player       (adv:find-player "player" my-world)))
+
+    (catch 'adv::escape-from-game
+      (adv::inner-game-repl
+       player
+       :input inputstream
+       :output outputstream))
+    
+    (let* ((the-output (get-output-stream-string outputstream)))
+      (when tracep
+	(format *standard-output* "~% The input we gave is   ~s" inputstring)
+	(format *standard-output* "~%    The output are expecting is  ~s" expected-output)
+	(format *standard-output* "~%    The output we got was ~s" the-output))
+      
+      (assert-true (search expected-output the-output))
+      the-output)))
 
 
 (defun initialize-fixture (&key (input *standard-input*) (output *standard-output*))
@@ -59,51 +84,27 @@
       ))   
     current-world))
   
-(defun run-testgame ()
-  "Run a test game interactively"
-  (let* ((my-world     (initialize-fixture))
-	 (player       (adv:find-player "player" my-world)))
-    (game-repl player)))
 
+;; Documentation can be found her
+;; file:///Users/rmz2/quicklisp/dists/quicklisp/software/lisp-unit-20120107-git/documentation/lisp-unit.html#overview
 
-(defun run-command-oneliner (inputstring expected-output &key (tracep nil))
-  "Run a sequence of game commands encoded in the inputstream and return the output from the game as a string"
-  (let* ((inputstream (make-string-input-stream (format nil "~a ~%quit~%" inputstring)))
-         (outputstream (make-string-output-stream))
-	 (my-world     (initialize-fixture :input inputstream :output outputstream))
-	 (player       (adv:find-player "player" my-world)))
-
-    (catch 'adv::escape-from-game
-      (adv::inner-game-repl
-       player
-       :input inputstream
-       :output outputstream))
-    
-    (let* ((the-output (get-output-stream-string outputstream)))
-      (when tracep
-	(format *standard-output* "~% The input we gave is   ~s" inputstring)
-	(format *standard-output* "~%    The output are expecting is  ~s" expected-output)
-	(format *standard-output* "~%    The output we got was ~s" the-output))
-      
-      (assert-true (search expected-output the-output))
-      the-output)))
 
 (define-test test-quit-emptyness
-  (run-command-oneliner "" ""))
+  (run-command-oneliner #'initialize-fixture "" ""))
 
 (define-test test-quit-cmd
-  (run-command-oneliner "quit" "Ttfn"))
+  (run-command-oneliner  #'initialize-fixture  "quit" "Ttfn"))
 
 (define-test test-look-for-sword
-  (run-command-oneliner "look" "sword of generic strikes"))
+  (run-command-oneliner  #'initialize-fixture  "look" "sword of generic strikes"))
 
 (define-test test-look-for-the-start
-  (run-command-oneliner "look" "The start"))
+  (run-command-oneliner  #'initialize-fixture  "look" "The start"))
 
 (define-test test-take-sword
-  (run-command-oneliner "take sword" "Got it"))
+  (run-command-oneliner  #'initialize-fixture  "take sword" "Got it"))
 
 (define-test test-take-sword-then-strike-monster
-  (run-command-oneliner "take sword
+  (run-command-oneliner  #'initialize-fixture  "take sword
 kill monster" "You are dead"))
 
