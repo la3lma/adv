9a6cd3e2bc67f05b0905e76cf0e5090be99f100c
Bjorn Remseth <la3lma@gmail.com>
2012-04-09 16:21:48 +0200

    A working, but increasingly weird buildfile

--- bin/buildall.sh
+++ bin/buildall.sh
@@ -12,12 +12,15 @@
 etags $(find . -name '*.lisp')
 
 ## This should have been sufficient but isn't
- clisp -q -x '(asdf:compile-system :adv)'
+clisp -q -x '(asdf:compile-system :adv)'
+clisp -q -x '(asdf:compile-system :adv-regression)'
+clisp -q -x '(asdf:compile-system :adv-regression-scenario1)'
 
 ## So instead we do this
 clisp -q -c "src/adv.lisp"
 clisp -q -c "src/adv-regression.lisp"
+clisp -q -c "src/adv-regression-scenario1.lisp"
 
 ## And run regression tests
-clisp  -q -x '(load "src/adv")(load "src/adv-regression.lisp")(in-package :adv-regression)(run-tests)'
+clisp  -q -x '(load "src/adv")(load "src/adv-regression.lisp")(load "src/adv-regression-scenario1.lisp")(in-package :adv-regression-scenario1)(run-tests)'
 
