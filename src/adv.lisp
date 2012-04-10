2bee98290aa0d52f3f053da03a6dc5f3ba020d46
Bjorn Remseth <la3lma@gmail.com>
2012-04-11 00:27:18 +0200

    Make move-object visible from the outside

--- src/adv.lisp
+++ src/adv.lisp
@@ -22,7 +22,7 @@
 
 (defpackage :adv
   (:use :common-lisp)
-  (:export  :add-to-inventory :inner-game-repl :game-repl :find-player :inventory))
+  (:export  :add-to-inventory :inner-game-repl :game-repl :find-player :inventory :move-object))
   
 (in-package :adv)
 
